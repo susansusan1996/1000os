f4d8624dc1676738fbdb915ba552ffb2d4089aed
susan <p76141526@gs.ncku.edu.tw>
2025-10-19 13:18:58 +0800

    檔案分資料夾

--- run.sh
+++ run.sh
@@ -4,13 +4,18 @@ set -xue
 QEMU=qemu-system-riscv32
 
 # Path to clang and compiler flags
-CC=/opt/homebrew/opt/llvm/bin/clang  # Ubuntu users: use CC=clang
+CC=/opt/homebrew/opt/llvm/bin/clang
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib"
 
+# 加入 include 路徑 （在include時，才能直接用資料夾路徑）
+CFLAGS="$CFLAGS -I."
+
 # Build the kernel
-# 編譯原始碼
+# 編譯原始碼（分資料夾版本）
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-    kernel.c common.c
+    common/common.c \
+    trap/trap.c \
+    kernel.c
 
 # Start QEMU
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
