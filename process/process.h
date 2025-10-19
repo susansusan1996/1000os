b985fa1223444cb0e3ea348782fe809ed8da27a8
susan <p76141526@gs.ncku.edu.tw>
2025-10-19 18:27:46 +0800

    10. 行程（Process）上半部

---
+++ process/process.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#define PROCS_MAX 8       // Maximum number of processes
+
+#define PROC_UNUSED   0   // Unused process control structure
+#define PROC_RUNNABLE 1   // Runnable process
+
+
+//PCB
+struct process {
+    int pid;             // Process ID
+    int state;           // Process state: PROC_UNUSED or PROC_RUNNABLE
+    vaddr_t sp;          // Stack pointer
+    uint8_t stack[8192]; // Kernel stack
+};
+
+struct process *create_process(uint32_t pc);
+
+void proc_a_entry(void);
+
+void proc_b_entry(void);
+
+extern struct process *proc_a;
+extern struct process *proc_b;
+
+#define PROC_A proc_a
+#define PROC_B proc_b
